7f38f6d272d655b289ceb698a95c9262ec549604
Johnny Chinchajoa <jact1001@gmail.com>
2023-02-22 19:40:21 -0500

    connect with mongodb

--- packages/forms-api/src/core/domain/IForm.ts
+++ packages/forms-api/src/core/domain/IForm.ts
@@ -1,42 +1,16 @@
-import {Model, ObjectID} from "@tsed/mongoose";
-import {Property} from "@tsed/schema";
+import {Model, ObjectID, Schema} from "@tsed/mongoose";
+import {CollectionOf, Property} from "@tsed/schema";
 
-@Model()
-export class IForm {
-    @ObjectID("id")
-    _id: string;
+@Schema()
+export class InputValue {
     @Property()
-    formName: string;
-    @Property()
-    sections: Section[];
-}
-
-@Model()
-export class Section {
-    @ObjectID("id")
-    _id: string;
-    @Property()
-    permissions: Permissions[];
-    @Property()
-    fields: Field[];
-}
-
-@Model()
-export class Permissions {
-    @ObjectID("id")
-    _id: string;
-    @Property()
-    roleId: string;
+    id: string;
     @Property()
     text: string;
-    @Property()
-    permission: string[];
 }
 
-@Model()
+@Schema()
 export class Field {
-    @ObjectID("id")
-    _id: string;
     @Property()
     isRequired: boolean;
     @Property()
@@ -44,12 +18,37 @@ export class Field {
     @Property()
     label: string;
     @Property()
-    values?: {
-        id: string,
-        text: string
-    };
+    values?: InputValue
     @Property()
     placeholder: string;
     @Property()
     maxLength: string;
+}
+
+@Schema()
+export class Permissions {
+    @Property()
+    roleId: string;
+    @Property()
+    text: string;
+    @CollectionOf(String)
+    permission: string[];
+}
+
+@Schema()
+export class Section {
+    @CollectionOf(Permissions)
+    permissions: Permissions[];
+    @CollectionOf(Field)
+    fields: Field[];
+}
+
+@Model()
+export class Form {
+    @ObjectID("id")
+    _id: string;
+    @Property()
+    formName: string;
+    @CollectionOf(Section)
+    sections: Section[];
 }
--- packages/forms-api/src/infraestructure/repository/form-repository.ts
+++ packages/forms-api/src/infraestructure/repository/form-repository.ts
@@ -1,21 +1,16 @@
 import {Inject, Injectable, OnDestroy} from "@tsed/common";
 import {MongooseModel} from "@tsed/mongoose";
-import {Test} from "../../core/domain/ITest";
-import {IForm} from "../../core/domain/IForm";
+import {Form} from "../../core/domain/IForm";
 
 @Injectable()
 export class FormRepository implements OnDestroy {
-    @Inject(Test)
-    private model: MongooseModel<Test>;
+    @Inject(Form)
+    private model: MongooseModel<Form>;
 
     public async findForm () {
         const list = await this.model.find().exec();
-
-        console.log('ejecutando consulta');
-
-        console.log(list); // Model { Product }
+        // Model { Product }
         //console.log(list[0].toClass()); // Product {}
-
         return list;
     }
 
