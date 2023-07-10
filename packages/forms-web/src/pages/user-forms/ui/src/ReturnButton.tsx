f49884f03e0be56e1098e22237b8d8deb492f727
Daniel50726 <62769266+Daniel50726@users.noreply.github.com>
2023-07-10 15:42:20 -0500

    added user-forms styles

--- packages/forms-web/src/pages/user-forms/ui/src/ReturnButton.tsx
+++ packages/forms-web/src/pages/user-forms/ui/src/ReturnButton.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import '../styles/return-button.scss';
-
 import ReturnIcon from "../icons/ReturnIcon";
 
 
--- packages/forms-web/src/pages/user-forms/ui/src/user-forms.tsx
+++ packages/forms-web/src/pages/user-forms/ui/src/user-forms.tsx
@@ -3,13 +3,16 @@ import '../styles/user-forms.scss';
 import {ReturnButton} from "./ReturnButton";
 import {FormTitle} from "./FormTitle";
 import {SlideFormsContainer} from "./SlideFormsContainer";
+import {HeadUserForm} from "./HeadUserForm";
 
 const defaultClass = 'user-forms-container';
+
 
 export const Userforms = () => {
 
     return (
         <div className={`${defaultClass}`}>
+            <HeadUserForm/>
             <div className={`${defaultClass}__forms-container`}>
                 <FormTitle/>
                 <SlideFormsContainer/>
