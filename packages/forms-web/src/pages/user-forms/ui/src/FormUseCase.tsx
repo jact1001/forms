d40cee02c6a7e10a9c3d276bb8625c720e356ed8
Johnny Chinchajoa <jact1001@gmail.com>
2023-10-25 14:38:50 -0500

    add button to cases

--- packages/forms-web/src/pages/user-forms/ui/src/FormUseCase.tsx
+++ packages/forms-web/src/pages/user-forms/ui/src/FormUseCase.tsx
@@ -12,9 +12,16 @@ interface SlideFormProps {
     }
 }
 export const FormUseCase = ({ formName, statusOption }: SlideFormProps) =>  {
+
+    const openCase = () => {
+        const domain = window.location.origin;
+        const url = `${domain}/form`;
+        window.open(url, '_blank');
+    }
+
     return (
         <div className={`${defaultClass}__button-content`}>
-            <button className={`${defaultClass}__button-option`}>
+            <button onClick={openCase} className={`${defaultClass}__button-option`}>
                 <div className={`${defaultClass}__name-status-container`}>
                     <b className={`${defaultClass}__name-status`}>{formName}</b>
                     <StatusUseCase statusCase={statusOption}/>
