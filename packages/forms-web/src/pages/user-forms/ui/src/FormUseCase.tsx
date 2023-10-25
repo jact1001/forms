d2567b715026497170737221f6f1af1146c284a7
Johnny Chinchajoa <jact1001@gmail.com>
2023-10-25 15:05:46 -0500

    id form and case id were added to form url

--- packages/forms-web/src/pages/user-forms/ui/src/FormUseCase.tsx
+++ packages/forms-web/src/pages/user-forms/ui/src/FormUseCase.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import '../styles/form-use-case.scss';
 import ArrowUpRightIcon from "../icons/ArrowUpRightIcon";
 import {StatusUseCase} from "./StatusUseCase";
+import { IFormCase } from "../../data/domain/IUserForms";
 
 const defaultClass = 'form-use-case';
 interface SlideFormProps {
-    formName:string
-    statusOption: {
-        id: string;
-        name: string;
-    }
+    formCase: IFormCase;
+    formId: string;
 }
-export const FormUseCase = ({ formName, statusOption }: SlideFormProps) =>  {
+export const FormUseCase = ({ formCase: {name, state, id}, formId }: SlideFormProps) =>  {
 
     const openCase = () => {
         const domain = window.location.origin;
-        const url = `${domain}/form`;
+        const url = `${domain}/form/${formId}/${id}`;
         window.open(url, '_blank');
     }
 
@@ -23,8 +21,8 @@ export const FormUseCase = ({ formName, statusOption }: SlideFormProps) =>  {
         <div className={`${defaultClass}__button-content`}>
             <button onClick={openCase} className={`${defaultClass}__button-option`}>
                 <div className={`${defaultClass}__name-status-container`}>
-                    <b className={`${defaultClass}__name-status`}>{formName}</b>
-                    <StatusUseCase statusCase={statusOption}/>
+                    <b className={`${defaultClass}__name-status`}>{name}</b>
+                    <StatusUseCase statusCase={state}/>
                 </div>
                 <ArrowUpRightIcon/>
             </button>
