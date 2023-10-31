ad0c4a1c4410ab2f2b58a16690e9d4f6dd69cb01
patinoricar <patinoricar@gmail.com>
2023-10-31 17:03:49 -0500

    :label: add button in footer

--- packages/forms-web/src/pages/form/data/state/actions/form.actions.ts
+++ packages/forms-web/src/pages/form/data/state/actions/form.actions.ts
@@ -1,10 +1,28 @@
+import { IForm } from "../../domain/IForm";
 import { TField } from "../../domain/IFormFields";
 
 export enum ActionType {
+    UPDATE_FORM_PENDING = 'UPDATE_FORM_PENDING',
+    UPDATE_FORM_SUCCESS = 'UPDATE_FORM_SUCCESS',
+    UPDATE_FORM_FAIL = 'UPDATE_FORM_FAIL',
     QUERY_FORM_PENDING = 'QUERY_FORM_PENDING',
     QUERY_FORM_SUCCESS = 'QUERY_FORM_SUCCESS',
     QUERY_FORM_FAIL = 'QUERY_FORM_FAIL',
     UPDATE_FORM_FIELD = 'UPDATE_FORM_FIELD'
+}
+
+interface updateActionPending {
+    type: ActionType.UPDATE_FORM_PENDING;
+}
+
+interface updateActionSuccess {
+    type: ActionType.UPDATE_FORM_SUCCESS;
+    payload: IForm;
+}
+
+interface updateActionFail {
+    type: ActionType.UPDATE_FORM_FAIL;
+    payload: string;
 }
 
 interface actionPending {
@@ -26,4 +44,11 @@ interface actionUpdateFormField {
     payload: {sectionId:string, field: TField};
 }
 
-export type Action = actionPending | actionSuccess | actionFail | actionUpdateFormField;
+export type Action = 
+    updateActionPending |
+    updateActionSuccess |
+    updateActionFail |
+    actionPending | 
+    actionSuccess | 
+    actionFail | 
+    actionUpdateFormField;
