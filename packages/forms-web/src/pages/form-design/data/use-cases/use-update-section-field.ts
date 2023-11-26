d5d65199a4a3be39f3fb19cd185a9a7ad1b1dc53
Johnny Chinchajoa <jact1001@gmail.com>
2023-11-26 14:07:35 -0500

    radio button was fixed

--- packages/forms-web/src/pages/form-design/data/use-cases/use-update-section-field.ts
+++ packages/forms-web/src/pages/form-design/data/use-cases/use-update-section-field.ts
@@ -4,13 +4,15 @@ import {ISection} from "../domain/IForm";
 export const updateSectionField = (sectionId: string, field: TField, sections: ISection[]) => {
     return sections.map((section) => {
         if (section.id === sectionId) {
-            section.fields.map((saveField: TField) => {
-                if (saveField.form_field_id === field.form_field_id) {
-                    return field;
-                }
-                return saveField;
-            });
-            return section;
+            return {
+                ...section,
+                fields: section.fields.map((saveField: TField) => {
+                    if (saveField.form_field_id === field.form_field_id) {
+                        return field;
+                    }
+                    return saveField;
+                })
+            }
         }
         return section;
     });
