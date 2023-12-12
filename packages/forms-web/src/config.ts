5ea175b6a3ec1b6e7bc354ceb06c475c36d13ab8
Ivan <ir.taimal10@uniandes.edu.co>
2023-12-11 22:32:00 -0500

    💥 fix REACT_APP_CLIENT_ID

--- packages/forms-web/src/config.ts
+++ packages/forms-web/src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_PATH = process.env.API_BASE_PATH ||  'http://localhost:8080/api'
+export const API_BASE_PATH = process.env.API_BASE_PATH ||  'https://2508f9ed227825ce37d62a5290e48165.yl4.us-east-2.eks.amazonaws.com/api'
