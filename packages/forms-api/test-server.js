0a972f620e79a230d577113bd2154cfb7d8f345a
Ivan <ir.taimal10@uniandes.edu.co>
2024-01-23 22:15:32 -0500

    💥 for test

--- packages/forms-api/test-server.js
+++ packages/forms-api/test-server.js
@@ -11,101 +11,20 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const OS = require('os');
-const ENV = process.env.GOOGLE_CLIENT_ID;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
   const msg = 'Hello from dynamic forms, this was awesome';
-  res.send(getPage(msg));
+  res.send(msg);
 });
 
 app.get('/test', (req, res) => {
   res.statusCode = 200;
   const msg = 'Hello from /test Node!';
-  res.send(getPage(msg));
+  res.send(msg);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-function getPage(message) {
-  const result = consumeCPU();
-  let body = "<!DOCTYPE html>\n"
-    + "<html>\n"
-    + "<style>\n"
-    + "body, html {\n"
-    + "  height: 100%;\n"
-    + "  margin: 0;\n"
-    + "}\n"
-    + "\n"
-    + ".bgimg {\n"
-    + "  background-image: url('https://www.w3schools.com/w3images/forestbridge.jpg');\n"
-    + "  height: 100%;\n"
-    + "  background-position: center;\n"
-    + "  background-size: cover;\n"
-    + "  position: relative;\n"
-    + "  color: white;\n"
-    + "  font-family: \"Courier New\", Courier, monospace;\n"
-    + "  font-size: 25px;\n"
-    + "}\n"
-    + "\n"
-    + ".topleft {\n"
-    + "  position: absolute;\n"
-    + "  top: 0;\n"
-    + "  left: 16px;\n"
-    + "}\n"
-    + "\n"
-    + ".bottomleft {\n"
-    + "  position: absolute;\n"
-    + "  bottom: 0;\n"
-    + "  left: 16px;\n"
-    + "}\n"
-    + "\n"
-    + ".middle {\n"
-    + "  position: absolute;\n"
-    + "  top: 50%;\n"
-    + "  left: 50%;\n"
-    + "  transform: translate(-50%, -50%);\n"
-    + "  text-align: center;\n"
-    + "}\n"
-    + "\n"
-    + "hr {\n"
-    + "  margin: auto;\n"
-    + "  width: 40%;\n"
-    + "}\n"
-    + "</style>\n"
-    + "<body>\n"
-    + "\n"
-    + "<div class=\"bgimg\">\n"
-    + "  <div class=\"topleft\">\n"
-    + "    <p>result: " + result +"</p>\n"
-    + "  </div>\n"
-    + "  <div class=\"middle\">\n"
-    + "    <h1>Host/container name</h1>\n"
-    + "    <hr>\n"
-    + "    <p>" + OS.hostname() + "</p>\n"
-    + "  </div>\n"
-    + "  <div class=\"bottomleft\">\n"
-    + "    <p>" + message + "</p>\n"
-    + "  </div>\n"
-    + "</div>\n"
-    + "\n"
-    + "</body>\n"
-    + "</html>\n";
-  return body;
-}
-
-function consumeCPU() {
-  const start = Date.now();
-  const duration = 5000; // Duración en milisegundos (5 segundos en este caso)
-  let result = 0;
-  while (Date.now() - start < duration) {
-    // Realiza cálculos intensivos (simulación de carga de CPU)
-    // Aquí puedes poner cualquier tarea que requiera tiempo de CPU
-     result = Math.pow(Math.random(), Math.random());
-  }
-  return result
-}
