a22e4a5d1b1b57c4006b5128d7bf0327882ae97d
Ivan <ir.taimal10@uniandes.edu.co>
2024-01-23 23:39:50 -0500

    💥 for test

--- packages/forms-web/src/config.ts
+++ packages/forms-web/src/config.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_PATH = process.env.REACT_APP_API_KEY || 'http://localhost:8080/api';
-export const API_BASE_PATH = 'https://a06e877c4e214441882a00be6ce84618-1162682444.us-east-2.elb.amazonaws.com/api';
+export const API_BASE_PATH = 'http://a06e877c4e214441882a00be6ce84618-1162682444.us-east-2.elb.amazonaws.com/api';
 // pequeño cambio
