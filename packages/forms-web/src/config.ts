f21f4191714bfa3b2072ddc608bcba6e0c0cc3c1
Ivan <ir.taimal10@uniandes.edu.co>
2024-01-24 21:57:03 -0500

    💥 for test

--- packages/forms-web/src/config.ts
+++ packages/forms-web/src/config.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_PATH = process.env.REACT_APP_API_KEY || 'http://localhost:8080/api';
-export const API_BASE_PATH = 'https://dsb471zsol61i.cloudfront.net/api';
+export const API_BASE_PATH = 'https://d19owaku1lqeqg.cloudfront.net/api';
 // pequeño cambio,
