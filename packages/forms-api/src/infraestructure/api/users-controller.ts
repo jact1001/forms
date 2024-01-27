9a0c52391884245bed9a034e8b45490f371e1294
Ivan <ir.taimal10@uniandes.edu.co>
2024-01-27 08:42:31 -0500

    💥 for test

--- packages/forms-api/src/infraestructure/api/users-controller.ts
+++ packages/forms-api/src/infraestructure/api/users-controller.ts
@@ -1,8 +1,8 @@
 import {Context, Controller, Get, Post, Request, Response, UseBefore} from "@tsed/common";
-import { IUser } from "../../core/domain/user";
-import { UsersUseCase } from "../../core/use-cases/users-use-case";
-import { Response as ExpressResponse } from "express";
-import { OAuth2Client } from "google-auth-library";
+import {IUser} from "../../core/domain/user";
+import {UsersUseCase} from "../../core/use-cases/users-use-case";
+import {Response as ExpressResponse} from "express";
+import {OAuth2Client} from "google-auth-library";
 import * as jwt from 'jsonwebtoken';
 import {AuthTokenMiddleware} from "../middlewares/auth-middleware";
 
@@ -12,7 +12,9 @@ const SECRET_KEY = process.env.SECRET_KEY || '';
 @Controller("/users")
 export class UsersController {
 
-    public constructor(private readonly _usersUseCase: UsersUseCase) {}
+    public constructor(private readonly _usersUseCase: UsersUseCase) {
+    }
+
     private googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
 
     @UseBefore(AuthTokenMiddleware)
@@ -25,23 +27,38 @@ export class UsersController {
     @Post("/login")
     async login(@Request() request, @Context() ctx: Context, @Response() res: ExpressResponse): Promise<any> {
         const token = request.headers["authorization"].split(" ")[1];
+        let step = '0'
+        let loginToken: any = "empty";
         try {
             const ticket = await this.googleClient.verifyIdToken({
                 idToken: token,
-                audience:GOOGLE_CLIENT_ID,
+                audience: GOOGLE_CLIENT_ID,
             });
-            const { email, name, family_name } = ticket.getPayload();
-            const loginToken = jwt.sign(`${email}`, SECRET_KEY);
-            await this._usersUseCase.saveUser({ email: email, user_name: name, last_name: family_name });
+            step = '1'
+            const {email, name, family_name} = ticket.getPayload();
+            step = '2'
+            loginToken = jwt.sign(`${email}`, SECRET_KEY);
+            step = '3'
+            await this._usersUseCase.saveUser({email: email, user_name: name, last_name: family_name});
+            step = '4'
             res.cookie("login", loginToken, {
                 httpOnly: true,
                 maxAge: 3600000,
             });
+            step = '5'
             return res.status(200).json({success: true, session: loginToken});
-        } catch (error) {-
+        } catch (error) {
             console.log('Token:', token);
             console.log('GOOGLE_CLIENT_ID:', GOOGLE_CLIENT_ID);
-            return res.status(403).json({ error: 'Token invalido. ' + error });
+            return res.status(403).json(
+                {
+                    error: 'Token invalido. ' + error,
+                    token: token,
+                    clientId: GOOGLE_CLIENT_ID,
+                    step: step,
+                    loginToken: loginToken
+                }
+            );
         }
     }
 
@@ -52,7 +69,7 @@ export class UsersController {
             ctx.delete("auth");
             return res.status(200).json('success');
         } catch (error) {
-            return res.status(501).json({ error: 'error' });
+            return res.status(501).json({error: 'error'});
         }
     }
 }
